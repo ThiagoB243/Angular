531ef6a66b7cc0449ba7518e9c11a86197e18c3f
Thiago <bortoluccicanal@gmail.com>
2021-12-10 16:58:21 -0300

    Adicionando layout do site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProdutosComponent } from './components/produtos/produtos.component';
+
+
+const routes: Routes = [{
+  path: "app-produtos",
+  component: ProdutosComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ProdutosComponent } from './components/produtos/produtos.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
