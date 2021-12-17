847b4271e726b79f217fcfa7bbef0d70fecfee86
Thiago <bortoluccicanal@gmail.com>
2021-12-16 21:25:56 -0300

    Adicionando imagens e componentes ao site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductScreenComponent } from './components/product-screen/product-screen.component';
 
 import { ProdutosComponent } from './components/produtos/produtos.component';
 
@@ -7,7 +11,22 @@ import { ProdutosComponent } from './components/produtos/produtos.component';
 const routes: Routes = [{
   path: "app-produtos",
   component: ProdutosComponent
-}];
+},
+{
+  path: "",
+  component: HomeComponent
+},{
+   path:"app-product-screen",
+   component: ProductScreenComponent
+}
+,{
+  path:"app-login",
+  component: LoginComponent
+},{
+  path:"app-cadastro",
+  component: CadastroComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ProdutosComponent } from './components/produtos/produtos.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductScreenComponent } from './components/product-screen/product-screen.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProdutosComponent
+    ProdutosComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductScreenComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
